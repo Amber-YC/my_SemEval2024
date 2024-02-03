b4074d8c1ee28f7d82619531f7142a63daa61b7c
yuchen <viveutvivasmyc@gmail.com>
2024-02-03 01:11:35 +0100

    amend preprocessing and adapters_model file

--- Model/adapters_model.py
+++ Model/adapters_model.py
@@ -1,10 +1,11 @@
 from transformers import AutoTokenizer, BertTokenizer
 from preprocessing import load_data
-from adapters import AutoAdapterModel, AdapterConfig, AdapterSetup, BnConfig
+from adapters import AutoAdapterModel, AdapterConfig, AdapterSetup, BnConfig, BertAdapterModel
 import numpy as np
 import torch.nn as nn
 import torch
 import adapters.composition as ac
+from datasets import Dataset
 # from transformers import TrainingArguments, AdapterTrainer, EvalPrediction
 # from sentence_transformers import BiEncoder
 
@@ -12,22 +13,12 @@ import adapters.composition as ac
 # Load tokenizer
 berttokenizer = BertTokenizer.from_pretrained("bert-base-multilingual-cased")
 
-def encode_batch(batch, tokenizer=berttokenizer):
-    """Encodes a batch of input data using the model tokenizer."""
-    text1_ls = batch['Text1'].tolist()
-    text2_ls = batch['Text2'].tolist()
-    tokenized_batch = tokenizer(text1_ls, text2_ls, padding=True, return_tensors="pt")
-    return tokenized_batch
-
-
 
 # Load pre-trained BERT model from Hugging Face Hub
 # The `BertAdapterModel` class is specifically designed for working with adapters
-model = AutoAdapterModel.from_pretrained("bert-base-multilingual-cased")
+bertmodel = BertAdapterModel.from_pretrained("bert-base-multilingual-cased", output_hidden_states=True)
 
-
-# add language layer
-def set_lang_adapter(lang: str, non_linearity="relu", reduction_factor=2):
+def set_lang_adapter(model, lang: str, non_linearity="relu", reduction_factor=2):
     """set language adapter for each language (eng, amh, arb, ind)
     parameters:
     - lang: pretrained-adapter name for each language,
@@ -48,42 +39,97 @@ def set_lang_adapter(lang: str, non_linearity="relu", reduction_factor=2):
     lang_adapter = model.load_adapter(lang, config=config, with_head=False) # language adapters are loaded but not activated
     print(f"{lang} language adapter loaded.")
     return lang_adapter
+# add model as parameter
 
-
-# add task adapter
-"""use single bottleneck adapter configuration"""
+"""add task adapter"""
 adapter_config = BnConfig(mh_adapter=False, output_adapter=True, reduction_factor=16, non_linearity="relu")
-task_adapter = model.add_adapter("STR", config=adapter_config)
-model.add_masked_lm_head("STR", activation_function="relu")
-model.train_adapter("STR")
+task_adapter = bertmodel.add_adapter("STR", config=adapter_config)
+# bertmodel.add_classification_head("STR", num_labels=3)
 print("task adapter added.")
 
-def get_word_embeddings(inputs, lang_adapter):
-    # stack and activate the certain language adapter and task adapter
-    with AdapterSetup(ac.Stack(lang_adapter, "STR")):
-        outputs = model(**inputs)
-    return outputs
-# 冻结la？如何train？
-# 是否可以使用 biencoder？
+# add language adapters
+eng_adapter = set_lang_adapter(bertmodel, "en/wiki@ukp")
+arb_adapter = set_lang_adapter(bertmodel, "ar/wiki@ukp")
+amh_adapter = set_lang_adapter(bertmodel, "am/wiki@ukp")
+ind_adapter = set_lang_adapter(bertmodel, "id/wiki@ukp")
+
+def encode_biencoder_batch(tidx, batch, tokenizer=berttokenizer):
+    """Encodes a batch of input data using the model tokenizer. tidx= 0 if text1, tidx=1 if text2"""
+    text = [pair[tidx] for pair in batch['pairs']]
+    encode_text = tokenizer(text, max_length=100, truncation=True, padding="max_length", return_tensors="pt")
+    return encode_text
+
+
+def get_biencoder_encoding(dataset):
+
+    t1_dataset = dataset.map(lambda x: encode_biencoder_batch(0, x), batched=True)
+    t2_dataset = dataset.map(lambda x: encode_biencoder_batch(1, x), batched=True)
+    dataset = dataset.add_column('t1_input_ids', t1_dataset["input_ids"])
+    dataset = dataset.add_column('t1_attention_mask', t1_dataset["attention_mask"])
+    # dataset = dataset.add_column('t1_token_type_ids', t1_dataset["token_type_ids"])
+    dataset = dataset.add_column('t2_input_ids', t2_dataset["input_ids"])
+    dataset = dataset.add_column('t2_attention_mask', t2_dataset["attention_mask"])
+    # dataset = dataset.add_column('t2_token_type_ids', t2_dataset["token_type_ids"])
+    if "Score" in dataset.column_names:
+        dataset = dataset.rename_column("Score", "labels")
+        dataset.set_format(type="torch", columns=['PairID', 't1_input_ids', 't1_attention_mask', 't2_input_ids', 't2_attention_mask', "labels"])
+    else:
+        dataset.set_format(type="torch", columns=['PairID', 't1_input_ids', 't1_attention_mask', 't2_input_ids', 't2_attention_mask'])
+
+    return dataset
+
+def encode_crossencoder_batch(batch, tokenizer=berttokenizer):
+    """Encodes a batch of input pairs using the model tokenizer for the cross encoder model."""
+    pairs = batch["pairs"]
+    encode_text = tokenizer(pairs, max_length=300, truncation=True, padding="max_length", return_tensors="pt")
+    return encode_text
+
+def get_crossencoder_encoding(dataset):
+    encode_dataset = dataset.map(lambda x: encode_crossencoder_batch(x), batched=True)
+    dataset = dataset.add_column('input_ids', encode_dataset["input_ids"])
+    dataset = dataset.add_column('attention_mask', encode_dataset["attention_mask"])
+
+    if "Score" in dataset.column_names:
+        dataset = dataset.rename_column("Score", "labels")
+        dataset.set_format(type="torch", columns=['PairID', 'input_ids', 'attention_mask', "labels"])
+    else:
+        dataset.set_format(type="torch", columns=['PairID', 'input_ids', 'attention_mask'])
+
+    return dataset
+
 
 
 if __name__ == "__main__":
-    train_file_eng = '../Semantic_Relatedness_SemEval2024-main/Track A/eng/eng_train.csv'
-    eng_data = load_data(train_file_eng)
-    eng_tokens = encode_batch(eng_data[:5])
-    print(eng_tokens['input_ids'])
-    print(eng_tokens['attention_mask'])
-    eng_adapter = set_lang_adapter("en/wiki@ukp")
-    # model.set_active_adapters(eng_adapter)
-    eng_output = get_word_embeddings(eng_tokens, eng_adapter)
-    print("eng_output:", eng_output)
+    train_file_eng = '../data/Track A/eng/eng_train.csv'
+    eng_test_data = load_data(train_file_eng)[:5]
+    eng_test_dataset = Dataset.from_pandas(eng_test_data)
+    eng_test_dataset = get_biencoder_encoding(eng_test_dataset)
+    eng_input_ids1, eng_attention_mask1, eng_input_ids2, eng_attention_mask2, eng_labels = eng_test_dataset['t1_input_ids'], \
+                                                                       eng_test_dataset['t1_attention_mask'], \
+                                                                       eng_test_dataset['t2_input_ids'], \
+                                                                       eng_test_dataset['t2_attention_mask'], \
+                                                                       eng_test_dataset['labels']
+    # print("Inputs shape: ",inputs.shape)
+    eng_adapter = set_lang_adapter(bertmodel, "en/wiki@ukp")
+    bertmodel.set_active_adapters(ac.Stack(eng_adapter, "STR"))
+    batch_output = bertmodel(eng_input_ids2, eng_attention_mask2, output_hidden_states=True)
+    last_hidden_state_shape = batch_output.hidden_states[-1].size()
+    print(batch_output)
+    print("eng_last_hidden_size", last_hidden_state_shape)
 
 
-    aim_file_ind = '../Semantic_Relatedness_SemEval2024-main/Track C/ind/ind_dev.csv'
-    ind_data = load_data(aim_file_ind)
-    ind_tokens = encode_batch(ind_data[:5])
-    ind_adapter = set_lang_adapter("id/wiki@ukp")
-    # model.set_active_adapters(ind_adapter) # 只激活这一个
-    ind_output = get_word_embeddings(ind_tokens, ind_adapter)
-    print("ind_output:", ind_output)
-
+    aim_file_ind = '../data/Track C/ind/ind_dev.csv'
+    ind_data = load_data(aim_file_ind)[:5]
+    ind_test_dataset = Dataset.from_pandas(ind_data)
+    ind_test_dataset = get_biencoder_encoding(ind_test_dataset)
+    ind_input_ids1, ind_attention_mask1, ind_input_ids2, ind_attention_mask2 = ind_test_dataset['t1_input_ids'], \
+                                                                       ind_test_dataset['t1_attention_mask'], \
+                                                                       ind_test_dataset['t2_input_ids'], \
+                                                                       ind_test_dataset['t2_attention_mask']
+    # print("Inputs shape: ",inputs.shape)
+    id_adapter = set_lang_adapter(bertmodel, "id/wiki@ukp")
+    bertmodel.set_active_adapters(ac.Stack(id_adapter, "STR"))
+    batch_output = bertmodel(ind_input_ids2, ind_attention_mask2, output_hidden_states=True)
+    last_hidden_state_shape = batch_output.hidden_states[-1].size()
+    print(batch_output)
+    print("ind_last_hidden_shape",  last_hidden_state_shape)
--- Model/preprocessing.py
+++ Model/preprocessing.py
@@ -5,66 +5,29 @@ import csv
 import re
 import sklearn
 from datasets import load_dataset
+from torch.utils.data import DataLoader
 
 def load_data(path):
     df = pd.read_csv(path)
-    df['Text1'] = df['Text'].apply(lambda x: re.split(r'\n|\t|\\n', x)[0])
-    df['Text2'] = df['Text'].apply(lambda x: re.split(r'\n|\t|\\n', x)[1])
+    df["pairs"] = df['Text'].apply(lambda x: re.split(r'\n|\t|\\n', x)) # easier to split training and val set
+    # always use .apply to deal with df columns/rows
     df.drop("Text", axis="columns")
     return df
 
-# def load_train_data(path):
-#     """load text and scores from file"""
-#     # with codecs.open(path, encoding='utf-8-sig') as f:
-#     #     sent_pairs = []
-#     #     score = []
-#     #     for row in csv.DictReader(f, skipinitialspace=True):
-#     #         sent_pair = re.split(r'\n|\t|\\n', row["Text"])
-#     #         sent_pairs.append(sent_pair)
-#     #         score.append(float(row['Score']))
-#     # return sent_pairs, score
-#     df = pd.read_csv(path)
-#     df['Text'] = df['Text'].apply(lambda x: re.split(r'\n|\t|\\n', x))
-#     return df
-#
-# def load_aim_data(path):
-#     """load text from trach C file"""
-#     with codecs.open(path, encoding='utf-8-sig') as f:
-#         sent_pairs = []
-#         for row in csv.DictReader(f, skipinitialspace=True):
-#             sent_pair = re.split(r'\n|\t|\\n', row["Text"])
-#             # sent_pair = row["Text"].split('\n')
-#             # print(sent_pair)
-#             sent_pairs.append(sent_pair)
-#
-#     return sent_pairs
-#
-#
+
+
 def get_batches(batch_size, data, shuffle=True):
-    total_data_size = len(data)
-    index_ls = [i for i in range(total_data_size)]
 
-    if shuffle:
-        dataset = sklearn.utils.shuffle(index_ls)
-
-    for start_i in range(0, total_data_size, batch_size):
-        # get batch_texts
-        end_i = min(total_data_size, start_i + batch_size)
-        batch_text_pairs = data[start_i:end_i]
-        yield batch_text_pairs
-
+    data_loader = DataLoader(data, batch_size=batch_size, shuffle=shuffle)
+    return data_loader
 
 
 
 if __name__ == '__main__':
-    train_file = '../Semantic_Relatedness_SemEval2024-main/Track A/eng/eng_train.csv'
-    test_file = '../Semantic_Relatedness_SemEval2024-main/Track C/amh/amh_dev.csv'
+    train_file = '../data/Track A/eng/eng_train.csv'
+    test_file = '../data/Track C/amh/amh_dev.csv'
 
     train_data = load_data(train_file)
     test_data = load_data(test_file)
-    print(train_data["Text2"][:5])
+    print(train_data["pairs"][:5])
     print(test_data)
-
-
-
-
